1a7ba1a5d94d736e814788f809ada1250ed03736
GodsaintBHH26 <kabirajarijit40@gmail.com>
2025-04-09 20:10:29 +0530

    made some big changes that are not visible from outside

--- src/play_section/ViewPort.jsx
+++ src/play_section/ViewPort.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useState } from "react";
 
 import PlayButtons from "./buttons";
 
 function ViewPort() {
+    const [animate, setAnimate] = useState(false);
+    const player = useRef(null)
+
+    
+      useEffect(()=>{
+        const pl = player.current
+        if(animate){
+          pl.classList.add('hitting')
+        }
+
+        const handleAnimationEnd = ()=>{
+          pl.classList.remove('hitting');
+          setAnimate(false);
+        }
+
+        pl.addEventListener('animationend', handleAnimationEnd);
+      },[animate]);
+    
+    
   return (
     <>
-      <div className="h-96 w-140 bg-gray-600 mb-5 border-4 rounded-md border-l-red-400 border-r-amber-500 border-t-yellow-600 border-b-pink-300"></div>
-      <PlayButtons/>
+      <div className="h-96 w-140 bg-gray-600 mb-5 border-4 rounded-md border-l-red-400 border-r-amber-500 border-t-yellow-600 border-b-pink-300 relative">
+        <div className="bg-white h-24 w-20 bottom-10 absolute left-20" ref={player}></div>
+        <div className="bg-white h-24 w-20 top-10 absolute right-20"></div>
+      </div>
+      <PlayButtons setAnimate={setAnimate}/>
     </>
   );
 }
--- src/play_section/buttons.jsx
+++ src/play_section/buttons.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-function PlayButtons() {
+function PlayButtons({setAnimate}) {
+  
+  const handleClick = (btnType)=>{
+    console.log(`Clicked ${btnType} button`);
+    setAnimate(true);
+  }
+
   return (
     <>
       <div className="w-140 border-3 border-gray-400 h-36 rounded-lg gap-2 p-2 grid grid-cols-2">
-        <button className="bg-blue-600">Mele attack</button>
-        <button className="bg-green-600">Light attack</button>
-        <button className="bg-amber-600">Special attack</button>
-        <button className="bg-yellow-600">Finisher</button>
+        <button className="bg-blue-600" onClick={()=>{handleClick('mele')}}>
+          Mele attack
+        </button>
+        <button className="bg-green-600" onClick={()=>{handleClick('reversal')}}>
+          Reversal
+        </button>
+        <button className="bg-amber-600" onClick={()=>{handleClick('special')}}>
+          Special attack
+        </button>
+        <button className="bg-yellow-600" onClick={()=>{handleClick('finisher')}}>
+          Finisher
+        </button>
       </div>
     </>
   );
